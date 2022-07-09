697ba0aac39ac1ec367b29ce8cdec335aba98718
Воронин Владимир <vladimirvoron7@gmail.com>
2022-07-09 19:47:05 +0300

    -

--- HTSDatabase/ReadAPI/hts_db_read.py
+++ HTSDatabase/ReadAPI/hts_db_read.py
@@ -1,2 +1,26 @@
+from APIServer.Cashing.DB_Entities.user import UserEntity
+from HTSDatabase.db_response import DBresponce
+
+
 class ReadDB:
-    pass
+    @staticmethod
+    def is_user_existent_by_name(db, name):
+        col = db['users']
+        if len(list(col.find({'name': name}))) != 0:
+            return True
+        return False
+
+    @staticmethod
+    def user_info_by_name(db, name):
+        col = db['users']
+        user = col.find_one({'name': name})
+        if len(list(user)) == 0:
+            return DBresponce(False, 'There is no user with this name')
+
+        user_ent = UserEntity(user['_id'], user['name'], user['salt'], user['hash_password'])
+        db_responce = DBresponce(True)
+        db_responce.info = {'User_entity': user_ent}
+
+        return db_responce
+
+
